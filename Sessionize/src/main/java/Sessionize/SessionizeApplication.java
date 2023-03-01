7bfdc0624743c41ab812f358c1bc00897f330f0e
Abdelhamid <abdelhamidbassou@gmail.com>
2023-03-01 10:19:51 +0100

    initalization

---
+++ Sessionize/src/main/java/Sessionize/SessionizeApplication.java
@@ -0,0 +1,13 @@
+package Sessionize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionizeApplication.class, args);
+	}
+
+}
